2bf9468cfb99f7afc504a6232945ce0c9593c17c
Tamarozi <cauaptamarozi@gmail.com>
2025-03-25 21:51:38 -0300

    Add files via upload

--- ex01.c
+++ ex01.c
@@ -1,17 +1,17 @@
 #include "stdio.h"
 
 int main() {
-    int num;
+    int num, soma;
     
     printf("Digite um número: ");
     scanf("%d", &num);
     
     int fim = num;
-    num = 0;
 
 for(int num = 1; num <= fim; num++) {
-  if (num % 2 != 0) {
-      printf("%d ", num);
-  }
+    if (num % 2 != 0) {
+        num += soma;
+        printf("%d ", soma);
+    }
 }
 }
