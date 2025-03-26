27d3f565523bb9eacf91aa9e1aebe309488f264d
Tamarozi <cauaptamarozi@gmail.com>
2025-03-25 22:03:06 -0300

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+  char tamanho;
+
+  printf("Digite o tamanho recebido, F para encerrar:");
+  tamanho = getchar();
+
+  int quantidadeP = 0;
+  int quantidadeM = 0;
+  int quantidadeG = 0;
+
+  while( tamanho != 'F' ) {
+    switch (tamanho) {
+      case 'P':
+	quantidadeP++;
+	break;
+      case 'M': 
+	quantidadeM++;
+	break;
+      case 'G':
+	quantidadeG++;
+	break;
+    }
+
+    printf("Digite o tamanho recebido, F para encerrar:");
+    tamanho = getchar();
+  }
+
+  printf("P: %d M: %d G: %d", quantidadeP, quantidadeM, quantidadeG);
+  return 0;
+}
